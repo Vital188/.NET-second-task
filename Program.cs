9271c3845d115e0b0c1121263d7794bc90dabef2
Vitalij <vitalij.cernysiov@gmail.com>
2022-12-02 17:59:38 +0200

    Program structure

--- Program.cs
+++ Program.cs
@@ -1,2 +1,64 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+namespace Recursion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter number n: ");
+            int x = Convert.ToInt32(Console.ReadLine());
+            // int x = 6;
+            case1(x);
+            Console.ReadKey();
+        }
+        static void case1(int n)
+        {
+            if (n > 5)
+            {
+                Console.Write($"Case 1: {n} ");
+                case1(n / 2);
+                case2(n + 3);
+            }
+            else 
+            {
+                Console.Write($"{n} is not right number, case 1");
+            }
+        }
+
+        static void case2(int n)
+        {
+            if (n > 0)
+            {
+                Console.Write($"{n} ");
+                case2(n - 1);
+            }
+        }
+
+        static void case3(int n)
+        {
+            if (n > 0)
+            {
+                Console.Write($"{n} ");
+                case3(n - 1);
+            }
+        }
+
+        static void case4(int n)
+        {
+            if (n > 0)
+            {
+                Console.Write($"{n} ");
+                case4(n - 1);
+            }
+        }
+
+        static void case5(int n)
+        {
+            if (n > 0)
+            {
+                Console.Write($"{n} ");
+                case5(n - 1);
+            }
+        }
+    }
+}
