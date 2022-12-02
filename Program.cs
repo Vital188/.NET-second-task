5f486eff5ac173bfc3070bedec34686ecd80f9f4
Vitalij <vitalij.cernysiov@gmail.com>
2022-12-02 19:28:36 +0200

    final structure

--- Program.cs
+++ Program.cs
@@ -5,60 +5,110 @@ namespace Recursion
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Enter number n: ");
+            Console.Write("Welcome to math test. First exercise, please, enter decision 5 * 8 =  ");
             int x = Convert.ToInt32(Console.ReadLine());
-            // int x = 6;
             case1(x);
             Console.ReadKey();
         }
         static void case1(int n)
         {
-            if (n > 5)
+            if (n == 40)
             {
-                Console.Write($"Case 1: {n} ");
-                case1(n / 2);
-                case2(n + 3);
+                Console.Write($" Good answer! Next question, enter decision: {n} / 2 = ");
+                int z = Convert.ToInt32(Console.ReadLine());
+                case2(z);
+                Console.ReadKey();
             }
             else 
             {
-                Console.Write($"{n} is not right number, case 1");
+                Console.Write($"Is not right answer, enter decision: 40 * 10 = ");
+                int p = Convert.ToInt32(Console.ReadLine());
+                case2a(p);
+                Console.ReadKey();
             }
         }
 
-        static void case2(int n)
+        static void case2a(int p)
         {
-            if (n > 0)
+            if (p == 400)
             {
-                Console.Write($"{n} ");
-                case2(n - 1);
+                Console.Write($" Good answer. Your mark is 5 !");
+            }
+            else 
+            {
+                Console.Write($"Sorry, it's bad answer, your mark is 2. Next time will be better!");
             }
         }
 
-        static void case3(int n)
+        static void case2(int z)
         {
-            if (n > 0)
+            if (z == 20)
             {
-                Console.Write($"{n} ");
-                case3(n - 1);
+                Console.Write($" Good answer! Next question, enter decision: {z} + {z} = ");
+                int d = Convert.ToInt32(Console.ReadLine());
+                case3(d);
+                Console.ReadKey();
+            }
+            if (z == 19 || z == 21)
+            {
+                Console.Write($"Is not right answer. Next question, enter decision: 40 * 10 = ");
+                int p = Convert.ToInt32(Console.ReadLine());
+                case3a(p);
+                Console.ReadKey();
+            }
+            else 
+            {
+                Console.Write($"Thank you for participation. Your mark is 6!");
             }
         }
 
-        static void case4(int n)
+
+
+        static void case3a(int p)
         {
-            if (n > 0)
+            if (p == 400)
             {
-                Console.Write($"{n} ");
-                case4(n - 1);
+                Console.Write(" Good answer. Your mark is 7!");
+            }
+            else 
+            {
+                Console.Write("Sorry, it's bad answer, your mark is 6.");
             }
         }
 
-        static void case5(int n)
+        static void case3(int d)
         {
-            if (n > 0)
+            if (d == 40)
             {
-                Console.Write($"{n} ");
-                case5(n - 1);
+                Console.Write($" Good answer! Next question, enter decision: 48 - 10 / 2 = ");
+                int q = Convert.ToInt32(Console.ReadLine());
+                case4(q);
+                Console.ReadKey();
             }
+            else
+            {
+                Console.Write($"Sorry, it's bad answer, your mark is 8.");
+            }
+        }
+
+        static void case4(int q)
+        {
+            if (q == 43)
+            {
+                Console.Write($" Good answer! Last question, please, enter your name: ");
+                string k = Console.ReadLine();
+                case5(k);
+                Console.ReadKey();
+            }
+            else
+            {
+                Console.Write($"Sorry, it's bad answer, your mark is 9.");
+            }
+        }
+
+        static void case5(string k)
+        {
+                Console.Write($" Congratilation {k}, your mark is 10 !!!");
         }
     }
 }
